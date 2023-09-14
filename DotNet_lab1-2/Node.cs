52c0ba583966c39a1b2033841d61bcac81f96602
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 00:51:34 +0300

    Create node

---
+++ DotNet_lab1-2/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_lab1_2
+{
+    public class Node<T> : IComparable<T>
+        where T : IComparable<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Left { get; set; }
+        public Node<T> Right { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+        public int CompareTo(T other)
+        {
+            return Data.CompareTo(other);
+        }
+    }
+}
