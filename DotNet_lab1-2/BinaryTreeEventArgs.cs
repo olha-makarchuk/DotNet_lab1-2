45ffc007626f7ebf3ceb9d47975bf1041988e78d
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 00:51:11 +0300

    Create eventArgs

---
+++ DotNet_lab1-2/BinaryTreeEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_lab1_2
+{
+    public class BinaryTreeEventsArgs<T> : EventArgs
+    {
+        public T Value { get; }
+
+        public BinaryTreeEventsArgs(T value)
+        {
+            Value = value;
+        }
+    }
+}
