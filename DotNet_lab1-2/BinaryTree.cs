1b9b3d2766fccde3282ea39b79055bbe2f3cda59
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 01:07:34 +0300

    Add events and functions: contains, add;

--- DotNet_lab1-2/BinaryTree.cs
+++ DotNet_lab1-2/BinaryTree.cs
@@ -9,7 +9,56 @@ namespace DotNet_lab1_2
     public class BinaryTree<T>
         where T : IComparable<T>
     {
+        public event EventHandler<BinaryTreeEventsArgs<T>> ItemAdded;
+        public event EventHandler<BinaryTreeEventsArgs<T>> ItemContained;
+        public event EventHandler<BinaryTreeEventsArgs<T>> ItemRemoved;
+        public event EventHandler ItemCleaned;
+
         public Node<T> Root { get; private set; }
         public int Count { get; private set; }
+
+        public void Add(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Data cannot be null.");
+            }
+
+            if (Root == null)
+            {
+                Root = new Node<T>(data);
+                Count = 1;
+                ItemAdded?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
+                return;
+            }
+
+            Root.Add(data);
+            Count++;
+        }
+
+        public bool Contains(T data)
+        {
+            if (data == null)
+            {
+                throw new ArgumentNullException(nameof(data), "Data cannot be null.");
+            }
+
+            if (Root != null)
+            {
+                bool found = Root.Contains(data);
+
+                if (found)
+                {
+                    InvokeItemContained(data);
+                }
+                return found;
+            }
+
+            return false;
+        }
+        private void InvokeItemAdded(T data) => ItemAdded?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
+        private void InvokeItemContained(T data) => ItemContained?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
+        private void InvokeItemRemoved(T data) => ItemRemoved?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
+
     }
 }
