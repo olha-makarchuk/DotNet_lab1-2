5b93efc9ed0d0b6d459368f0c23da7fc7fde0eb6
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 00:51:58 +0300

    Create binary tree

---
+++ DotNet_lab1-2/BinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_lab1_2
+{
+    public class BinaryTree<T>
+        where T : IComparable<T>
+    {
+        public Node<T> Root { get; private set; }
+        public int Count { get; private set; }
+    }
+}
