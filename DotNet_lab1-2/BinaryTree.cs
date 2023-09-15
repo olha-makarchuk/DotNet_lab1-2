d2c34f12e751dd0ab8bbc3da0bc2674cb467d243
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 16:24:00 +0300

    Tree traversal

--- DotNet_lab1-2/BinaryTree.cs
+++ DotNet_lab1-2/BinaryTree.cs
@@ -56,9 +56,104 @@ namespace DotNet_lab1_2
 
             return false;
         }
+
+
+        public MyList<T> Preorder()
+        {
+            if (Root == null)
+            {
+                return new MyList<T>();
+            }
+
+            return Preorder(Root);
+
+        }
+
+        public MyList<T> Postorder()
+        {
+            if (Root == null)
+            {
+                return new MyList<T>();
+            }
+
+            return Postorder(Root);
+
+        }
+
+        public MyList<T> Inorder()
+        {
+            if (Root == null)
+            {
+                return new MyList<T>();
+            }
+
+            return Inorder(Root);
+        }
+
+        private MyList<T> Preorder(Node<T> node)
+        {
+            var list = new MyList<T>();
+            if (node != null)
+            {
+                list.Add(node.Data);
+
+                if (node.Left != null)
+                {
+                    list.AddRange(Preorder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Preorder(node.Right));
+                }
+            }
+            return list;
+        }
+
+        private MyList<T> Postorder(Node<T> node)
+        {
+            var list = new MyList<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(Postorder(node.Left));
+                }
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Postorder(node.Right));
+                }
+
+                list.Add(node.Data);
+            }
+            return list;
+        }
+
+        private MyList<T> Inorder(Node<T> node)
+        {
+            var list = new MyList<T>();
+            if (node != null)
+            {
+                if (node.Left != null)
+                {
+                    list.AddRange(Inorder(node.Left));
+                }
+
+                list.Add(node.Data);
+
+                if (node.Right != null)
+                {
+                    list.AddRange(Inorder(node.Right));
+                }
+            }
+            return list;
+        }
+
         private void InvokeItemAdded(T data) => ItemAdded?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
         private void InvokeItemContained(T data) => ItemContained?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
         private void InvokeItemRemoved(T data) => ItemRemoved?.Invoke(this, new BinaryTreeEventsArgs<T>(data));
+
 
     }
 }
