20f49af98aabc79bd76e9080746ae8852a09ae9b
Ольга Макарчук <olamacarchuk@gmail.com>
2023-09-15 16:22:17 +0300

    IEnumerable<T> realization

---
+++ DotNet_lab1-2/MyList.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNet_lab1_2
+{
+    public class MyList<T> : IEnumerable<T>
+    {
+        private T[] items;
+        private int count;
+
+        public MyList()
+        {
+            items = new T[4];
+            count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (count == items.Length)
+            {
+                Array.Resize(ref items, items.Length * 2);
+            }
+            items[count] = item;
+            count++;
+        }
+
+        public void AddRange(IEnumerable<T> collection)
+        {
+            foreach (var item in collection)
+            {
+                Add(item);
+            }
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
