cf38e0a8c05dd345a696cfe62217ea6b84a133c1
Ольга Макарчук <olamacarchuk@gmail.com>
2023-10-15 21:52:52 +0300

    ChageClassForTests

--- BinaryTreeCollectionTests/BinaryTreeTests.cs
+++ BinaryTreeCollectionTests/BinaryTreeTests.cs
@@ -6,39 +6,34 @@ namespace BinaryTreeCollectionTests
     {
         private static readonly int[] Numbers = { 4, 5, 1, 3, 2 };
         private static readonly string[] Letters = { "s", "ac", "ab", "p" };
-        private static readonly TestClass<int>[] TestObject = { new(4), new(6), new(1), new(3), new(0) };
+        private static readonly TestObject<int>[] Objects = { new(4), new(6), new(1), new(3), new(0) };
 
 
         public static IEnumerable<object[]> GetEmptyTreeTestData()
         {
             yield return new object[] { new BinaryTree<int>() };
             yield return new object[] { new BinaryTree<string>() };
-            yield return new object[] { new BinaryTree<TestClass<int>>() };
+            yield return new object[] { new BinaryTree<TestObject<int>>() };
         }
 
         public static IEnumerable<object[]> GetTreeData()
         {
             yield return new object[] { Numbers };
             yield return new object[] { Letters };
-            yield return new object[] { TestObject };
+            yield return new object[] { Objects };
         }
 
-        protected class TestClass<T> : IComparable<TestClass<T>>
+        protected class TestObject<T> : IComparable<TestObject<T>>
         {
             public int Value { get; set; }
 
-            public TestClass(int value)
+            public TestObject(int value)
             {
                 Value = value;
             }
 
-            public int CompareTo(TestClass<T> other)
+            public int CompareTo(TestObject<T> other)
             {
-                if (other == null)
-                {
-                    throw new ArgumentNullException(nameof(other));
-                }
-
                 return Value.CompareTo(other.Value);
             }
         }
