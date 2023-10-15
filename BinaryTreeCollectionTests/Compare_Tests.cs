7721586bd749bc343e3f4f468a9bb247f6137a06
Ольга Макарчук <olamacarchuk@gmail.com>
2023-10-15 21:35:37 +0300

    TestsForTheCompareMethod

---
+++ BinaryTreeCollectionTests/Compare_Tests.cs
@@ -0,0 +1,31 @@
+﻿using DotNet_lab1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeCollectionTests
+{
+    public class Compare_Tests
+    {
+        [Fact]
+        public void Compare_ThrowsArgumentException_WhenTDoesNotImplementIComparable()
+        {
+            var node = new Node<MyComparableClass>(new MyComparableClass(1));
+
+            var exception = Assert.Throws<ArgumentException>(() => node.Compare(new MyComparableClass(1), new MyComparableClass(2)));
+        }
+
+
+        public class MyComparableClass
+        {
+            public int Value { get; set; }
+
+            public MyComparableClass(int value)
+            {
+                Value = value;
+            }
+        }
+    }
+}
