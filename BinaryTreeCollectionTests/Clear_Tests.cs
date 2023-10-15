5d8dda17e487062cd13a697f0a5c2e5bb4d33702
Ольга Макарчук <olamacarchuk@gmail.com>
2023-10-15 21:35:23 +0300

    TestsForTheClearMethod

---
+++ BinaryTreeCollectionTests/Clear_Tests.cs
@@ -0,0 +1,26 @@
+﻿using DotNet_lab1_2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTreeCollectionTests
+{
+    public class Clear_Tests: BinaryTreeTests
+    {
+        [Theory]
+        [MemberData(nameof(GetTreeData))]
+        public void Clear_WhenClearedTree_ShouldMakeTreeEmpty<T>(T[] values)
+        {
+            var tree = new BinaryTree<T>();
+            foreach (var value in values)
+            {
+                tree.Add(value);
+            }
+            tree.Clear();
+
+            Assert.Empty(tree);
+        }
+    }
+}
